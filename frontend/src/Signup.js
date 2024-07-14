61dcb19e87f1af1ef25cf5c7aad9d37ecca45388
its5kay <bestwiar@gmail.com>
2024-07-14 17:19:57 +0900

    signup added (frontend)

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,39 +1,39 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './App.css';
 
-const Login = () => {
+const Signup = () => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
+  const handleSignup = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('/api/login', { id, password });
-      // 로그인 성공 시 처리 (예: 토큰 저장, 사용자 정보 저장 등)
+      const response = await axios.post('/api/signup', { id, password });
+      // 회원가입 성공 시 처리
       console.log(response.data);
-      navigate('/chatroom'); // 로그인 성공 후 채팅방 페이지로 이동
+      // 여기서 적절한 회원가입 성공 후 처리를 추가하세요 (예: 로그인 페이지로 이동)
+      navigate('/login');
     } catch (error) {
-      setError('잘못된 아이디 또는 비밀번호입니다.');
+      setError('회원가입에 실패했습니다.');
     }
   };
 
   return (
     <div className="login-container">
       <div className="login-form">
-        <h2>로그인</h2>
-        <form onSubmit={handleSubmit}>
+        <h2>회원가입</h2>
+        <form onSubmit={handleSignup}>
           <div>
             <input
               type="text"
               placeholder="아이디"
               value={id}
               onChange={(e) => setId(e.target.value)}
-              requred
+              required
             />
           </div>
           <div>
@@ -42,16 +42,16 @@ const Login = () => {
               placeholder="비밀번호"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              requred
+              required
             />
           </div>
           {error && <p>{error}</p>}
-          <button type="submit">로그인</button>
+          <button type="submit" class="signup-button">회원가입</button>
         </form>
-        <p><Link to="/signup" className="signup-button">회원가입</Link></p>
+        <p>이미 회원이신가요? <Link to="/login">로그인</Link></p>
       </div>
     </div>
   );
 }
 
-export default Login;
+export default Signup;
